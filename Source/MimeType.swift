81e99dc12daa071d1ca9902248b475eb8af26028
Md. Rashed - Uz - Zaman <rashedgit@gmail.com>
2021-02-13 18:15:37 +0600

    AlamofireKit Version - 0.1.1.02
    AlamofireKit Version - 0.1.1.02

--- Source/MimeType.swift
+++ Source/MimeType.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import MobileCoreServices
+import AVFoundation
 
 extension NSURL {
     public func getMimeType() -> String {
@@ -21,6 +23,63 @@ extension NSString {
 extension String {
     public func getMimeType() -> String {
         return (self as NSString).getMimeType()
+    }
+}
+
+extension URL {
+    func mimeType() -> String {
+        let pathExtension = self.pathExtension
+        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as NSString, nil)?.takeRetainedValue() {
+            if let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+                return mimetype as String
+            }
+        }
+        return "application/octet-stream"
+    }
+    var containsImage: Bool {
+        let mimeType = self.mimeType()
+        guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeImage)
+    }
+    var containsAudio: Bool {
+        let mimeType = self.mimeType()
+        guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeAudio)
+    }
+    var containsVideo: Bool {
+        let mimeType = self.mimeType()
+        guard  let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeMovie)
+    }
+    
+    //Get current mime type of url w.r.t its url
+    var currentMimeType: String {
+        
+        if self.containsImage{
+            return "image/png"
+        }else if self.containsAudio{
+            return "audio/mp3"
+        }else if self.containsVideo{
+            return "video/mp4"
+        }
+        
+        return ""
+    }
+    //https://stackoverflow.com/questions/31243371/path-extension-and-mime-type-of-file-in-swift
+}
+extension AVFileType {
+    /// Fetch and extension for a file from UTI string
+    var fileExtension: String {
+        if let ext = UTTypeCopyPreferredTagWithClass(self as CFString, kUTTagClassFilenameExtension)?.takeRetainedValue() {
+            return ext as String
+        }
+        return "None"
     }
 }
 //#if swift(>=5.3)
