6fd9b2a8745cb1d693dabdd87da87bde3ec615db
Md. Rashed - Uz - Zaman <rashedgit@gmail.com>
2021-02-13 12:04:25 +0600

    AlamofireKit Version - 0.1.1.01
    AlamofireKit Version - 0.1.1.01

---
+++ Source/MimeType.swift
@@ -0,0 +1,52 @@
+//
+//  MimeType.swift
+//  AlamofireKit
+//
+//  Created by Rz Rasel on 2021-02-13
+//
+
+import Foundation
+import MobileCoreServices
+import AVFoundation
+
+extension URL {
+    public func mimeType() -> String {
+        let pathExtension = self.pathExtension
+        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as NSString, nil)?.takeRetainedValue() {
+            if let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+                return mimetype as String
+            }
+        }
+        return "application/octet-stream"
+    }
+    public var containsImage: Bool {
+        let mimeType = self.mimeType()
+        guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeImage)
+    }
+    public var containsAudio: Bool {
+        let mimeType = self.mimeType()
+        guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeAudio)
+    }
+    public var containsVideo: Bool {
+        let mimeType = self.mimeType()
+        guard  let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        return UTTypeConformsTo(uti, kUTTypeMovie)
+    }
+}
+extension AVFileType {
+    /// Fetch and extension for a file from UTI string
+    public var fileExtension: String {
+        if let ext = UTTypeCopyPreferredTagWithClass(self as CFString, kUTTagClassFilenameExtension)?.takeRetainedValue() {
+            return ext as String
+        }
+        return "None"
+    }
+}
